bea702b43da06fbf7df556f3e9c275ea4f758eaf
Abe Reyes <abe.raise@gmail.com>
2023-11-28 18:46:02 -0500

    "Added Input field for name"

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      this.counter = this.counter - num;
+    }
   },
 });
 
